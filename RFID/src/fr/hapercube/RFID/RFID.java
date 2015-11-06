6894ddc27c7fbe5eb0bdf0a3860afda1abba0b98
Lezzio <allan.guigal0@gmail.com>
2015-11-06 02:19:57 +0100

    Found the way to read

--- RFID/src/fr/hapercube/RFID/RFID.java
+++ RFID/src/fr/hapercube/RFID/RFID.java
@@ -1,9 +1,14 @@
 package fr.hapercube.RFID;
 
+import java.util.Arrays;
+import java.util.List;
+
 import fr.hapercube.RFID.handler.RFIDHandler;
 
 public class RFID {
 	
+	public static final int BAUD_RATE = 115200;
+	public static final List<String> PORTS = Arrays.asList("COM3");
 	
 	public static void main(String ... args){
 		new RFIDHandler();
--- RFID/src/fr/hapercube/RFID/handler/RFIDHandler.java
+++ RFID/src/fr/hapercube/RFID/handler/RFIDHandler.java
@@ -1,8 +1,12 @@
 package fr.hapercube.RFID.handler;
 
+import java.io.IOException;
+
 import com.fazecast.jSerialComm.SerialPort;
 import com.fazecast.jSerialComm.SerialPortDataListener;
 import com.fazecast.jSerialComm.SerialPortEvent;
+
+import fr.hapercube.RFID.RFID;
 
 
 public class RFIDHandler {
@@ -13,20 +17,44 @@ public class RFIDHandler {
 		this.serialPort = serialPort;
 	}
 	public RFIDHandler(){
-		for(SerialPort port : SerialPort.getCommPorts())
-				System.out.println("Name: " + port.getDescriptivePortName() + " Opened: " + port.isOpen());
+		for(SerialPort port : SerialPort.getCommPorts()){
+				if(RFID.PORTS.contains(port.getSystemPortName()) && port.openPort()){
+					serialPort = port; 
+					System.out.println("Port found & opened: " + port.getDescriptivePortName());
+					break;
+				}
+				System.out.println("Checked -> Name: " + port.getDescriptivePortName() + " Baud: " + port.getBaudRate() + " Opened: " + port.isOpen());
+		}
+		if(serialPort != null){
+			serialPort.setBaudRate(RFID.BAUD_RATE);
+			serialPort.addDataListener(new SerialPortListener());
+		}
 	}
 	
 	public class SerialPortListener implements SerialPortDataListener {
 
 		@Override
 		public int getListeningEvents() {
-			return 0;
+			return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
 		}
 
 		@Override
 		public void serialEvent(SerialPortEvent event) {
-			
+			if(event.getEventType() == SerialPort.LISTENING_EVENT_DATA_RECEIVED){
+				SerialPort port = event.getSerialPort();
+				
+				final byte[] readBuffer = new byte[port.bytesAvailable()];
+				if(port.bytesAvailable() > 0){
+					try {
+						port.getInputStream().read(readBuffer);
+						String data = new String(readBuffer);
+						System.out.println(data);
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			} else return;
 		}
 		
 	}
