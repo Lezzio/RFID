bfd27a2981d8b4253590bcc7caf1ae58e2442f02
Lezzio <allan.guigal0@gmail.com>
2016-01-11 20:45:24 +0100

    [REF] ActionTreat new method -> use a split char

--- RFID Artery/src/fr/gap/rfid/actions/ActionTreat.java
+++ RFID Artery/src/fr/gap/rfid/actions/ActionTreat.java
@@ -3,43 +3,30 @@ package fr.gap.rfid.actions;
 import fr.gap.rfid.handler.RFIDAction;
 
 public class ActionTreat implements RFIDAction {
+
+	private String stringSplitter = new String();
+	private CharSequence split;
 	
-	StringBuilder stringBuilder = new StringBuilder();
-	
+	public ActionTreat(CharSequence split) {
+		this.split = split;
+	}
+
 	@Override
 	public void handle(String data) {
-		stringBuilder.append(data);
-		//Sort with hexadecimal value
-		String[] hexaData = stringBuilder.toString().split("0x");
-		
-		//Identify each product
-		if(hexaData.length >= 4) {
-			String productString = "";
 
-			//TODO End it
-		 for(String hexa : hexaData) {
-			 
-			 int a = 0;
-			 System.out.println(hexa);
-			 productString += hexa;
-			 
-			 if(a++ >= 4) {
-				 a = 0;
-				 retrieveProduct(productString);
-				 continue;
-			 }
-			 
-		 }
-			//Release memory
-			stringBuilder.delete(0, stringBuilder.length());
-		 
-		} 
+		stringSplitter += data;
+		if(stringSplitter.contains(split)) {
+			
+			for(String code : stringSplitter.split("*")) {
+				retrieveProduct(code);
+			}
+			
+		}
+
 	}
-	
-	public void retrieveProduct(String product){
+
+	public void retrieveProduct(String product) {
 		System.out.println("Product caught: " + product);
 	}
-	
-	
 
 }
