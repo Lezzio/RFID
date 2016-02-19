7e0da35fffbd1860ffba571e75ddf9856d943840
Lezzio <allan.guigal0@gmail.com>
2016-02-19 20:21:44 +0100

    Create a BoardManager (To improve)

---
+++ RFID Artery/src/fr/pag/rfid/board/BoardManager.java
@@ -0,0 +1,10 @@
+package fr.pag.rfid.board;
+
+import java.util.ArrayList;
+
+public class BoardManager {
+	
+	private ArrayList<Board> controllers = new ArrayList<Board>();
+	private ArrayList<Board> readers = new ArrayList<Board>();
+
+}
