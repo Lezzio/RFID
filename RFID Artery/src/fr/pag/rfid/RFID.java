c43c8cde1913026f0486c8a387b3b2b82d1dacf2
Lezzio <allan.guigal0@gmail.com>
2016-02-26 03:23:21 +0100

    Update main class (use introduced features)

--- RFID Artery/src/fr/pag/rfid/RFID.java
+++ RFID Artery/src/fr/pag/rfid/RFID.java
@@ -1,13 +1,16 @@
 package fr.pag.rfid;
 
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import fr.pag.rfid.async.ThreadPool;
 import fr.pag.rfid.board.Board;
 import fr.pag.rfid.board.BoardBuilder;
 import fr.pag.rfid.board.BoardRole;
+import fr.pag.rfid.database.IDatabase;
+import fr.pag.rfid.database.MongoAdapter;
+import fr.pag.rfid.database.SQLAdapter;
 import fr.pag.rfid.handler.actions.ActionReader;
 import fr.pag.rfid.handler.actions.ActionRole;
 
@@ -16,14 +19,24 @@ public class RFID {
 	public static final int BAUD_RATE = 115200;
 	public static final List<String> PORTS = Arrays.asList("COM3", "COM6");
 	
+	public static PrintWriter printWriter;
+	
+	//Hexagonal stuff
+	public static IDatabase sqlDatabase = new SQLAdapter();
+	public static IDatabase mongoDatabase = new MongoAdapter();
+	
+	
 	public static void main(String ... args) {
+		
+		//Uncomment for local debugging
+		printWriter = new PrintWriter(System.out, true);
 		
 		for(Board board : searchBoards(1)) {
 			
 			 //Let the board listening
 			board.setListening(true);
-			board.setRole(BoardRole.READER);
-			
+			board.execute(ActionRole.class, ActionRole.ASK_ROLE);
+			board.setRole(BoardRole.READER); //To remove soon
 		}
 		
 	}
